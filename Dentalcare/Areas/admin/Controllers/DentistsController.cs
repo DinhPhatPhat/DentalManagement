e5f95dbbebdb12baf54b2582cbb4d0675b68a13e
DinhPhatPhat <dinhtainang@gmail.com>
2024-12-02 23:45:43 +0700

    Them xoa sua Nha Si, (Chua check trung ten dang nhap nha si, giao dien sua nha si chua hien duoc birthday cua nha si, nhung update duoc

--- Dentalcare/Areas/admin/Controllers/DentistsController.cs
+++ Dentalcare/Areas/admin/Controllers/DentistsController.cs
@@ -7,18 +7,30 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using Dentalcare.Models;
+using Dentalcare.Areas.admin.Models;
+using System.IO;
+using System.Data.Entity.Validation;
 
 namespace Dentalcare.Areas.admin.Controllers
 {
     public class DentistsController : Controller
     {
         private clinicEntities db = new clinicEntities();
-
         // GET: admin/Dentists
         public ActionResult Index()
         {
-            var dentists = db.Dentists.Include(d => d.Faculty).Include(d => d.Person);
-            return View(dentists.ToList());
+            var dentists = db.Dentists
+                             .Include(d => d.Faculty)
+                             .Include(d => d.Person)
+                             .ToList();
+            // Chuyển đổi thành danh sách ViewModel
+            var dentistViewModels = dentists.Select(d => new CreateDentistViewModel
+            {
+                dentist = d,
+                person = d.Person,
+                account = db.Accounts.FirstOrDefault(a => a.id == d.Person.id) // Giả sử mối quan hệ giữa Account và Person là qua id
+            }).ToList();
+            return View(dentistViewModels);
         }
 
         // GET: admin/Dentists/Details/5
@@ -37,30 +49,120 @@ namespace Dentalcare.Areas.admin.Controllers
         }
 
         // GET: admin/Dentists/Create
+        
         public ActionResult Create()
         {
             ViewBag.falid = new SelectList(db.Faculties, "id", "name");
-            ViewBag.id = new SelectList(db.People, "id", "meta");
             return View();
         }
 
-        // POST: admin/Dentists/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        private string GenerateNewAccountId()
+        {
+            // Lấy accountId lớn nhất hiện có trong bảng Account
+            var lastAccountId = db.Accounts
+                                 .OrderByDescending(a => a.id) // Sắp xếp giảm dần theo id
+                                 .Select(a => a.id) // Lấy giá trị id
+                                 .FirstOrDefault(); // Lấy giá trị đầu tiên
+
+            // Nếu chưa có accountId nào, bắt đầu từ AC00000001
+            if (string.IsNullOrEmpty(lastAccountId))
+                return "AC00000001";
+
+            // Lấy phần số từ accountId, bỏ phần "AC"
+            int numberPart = int.Parse(lastAccountId.Substring(2));
+
+            // Tăng phần số lên 1
+            numberPart++;
+
+            // Trả về accountId mới với định dạng "ACXXXXXXXX"
+            return $"AC{numberPart:D8}";
+        }
+        // POST: Dentists/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "title,hide,meta,order,datebegin,id,falid,descrip")] Dentist dentist)
+        public ActionResult Create(CreateDentistViewModel model, HttpPostedFileBase avatar)
         {
             if (ModelState.IsValid)
             {
-                db.Dentists.Add(dentist);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                var newAccountId = GenerateNewAccountId();
+
+                // Tạo mới Account với userName và password
+                var account = new Account
+                {
+                    id = newAccountId,
+                    username = model.account.username,
+                    password = "123", // Sử dụng mật khẩu mặc định là "123"
+                    meta = newAccountId,
+                    hide = false,
+                    datebegin = DateTime.UtcNow,
+                    able = true,
+                };
+
+                // Xử lý upload file avatar
+                string avatarPath = "123";
+                if (avatar != null && avatar.ContentLength > 0)
+                {
+                    // Lấy đuôi file (ví dụ .jpg, .png)
+                    string fileExtension = Path.GetExtension(avatar.FileName);
+
+                    // Đường dẫn thư mục lưu trữ
+                    string folderPath = Server.MapPath($"~/Content/images/{newAccountId}/");
+
+                    // Tạo thư mục nếu chưa tồn tại
+                    if (!Directory.Exists(folderPath))
+                        Directory.CreateDirectory(folderPath);
+
+                    // Đường dẫn đầy đủ của file
+                    avatarPath = $"/Content/images/{newAccountId}/avatar{fileExtension}";
+
+                    // Lưu file lên server
+                    avatar.SaveAs(Path.Combine(folderPath, $"avatar{fileExtension}"));
+                }
+
+                // Tạo mới Person và lưu đường dẫn avatar vào trường img
+                var person = new Person
+                {
+                    id = newAccountId,
+                    name = model.person.name,
+                    phoneNumber = model.person.phoneNumber,
+                    email = model.person.email,
+                    salary = model.person.salary,
+                    address = model.person.address,
+                    gender = model.person.gender,
+                    birthday = model.person.birthday,
+                    nation = model.person.nation,
+                    img = avatarPath,  // Lưu đường dẫn avatar vào trường img
+                    meta = model.dentist.meta,
+                    role = 3,
+                    hide = false,
+                    datebegin = DateTime.UtcNow
+                };
+
+                // Tạo mới Dentist
+                var dentist = new Dentist
+                {
+                    id = newAccountId,
+                    title = model.dentist.title,
+                    hide = model.dentist.hide,
+                    meta = model.dentist.meta,
+                    descrip = model.dentist.descrip,
+                    falid = model.dentist.falid, // Lưu faculty ID
+                    datebegin = DateTime.UtcNow
+                };
+
+                // Lưu các đối tượng vào cơ sở dữ liệu
+             
+                    db.Accounts.Add(account);
+                    db.People.Add(person);
+                    db.Dentists.Add(dentist);
+                    db.SaveChanges();
+                
+                return RedirectToAction("Index"); // Chuyển hướng về danh sách Dentist
             }
 
-            ViewBag.falid = new SelectList(db.Faculties, "id", "name", dentist.falid);
-            ViewBag.id = new SelectList(db.People, "id", "meta", dentist.id);
-            return View(dentist);
+            // Nếu ModelState không hợp lệ, trả lại view với model để người dùng sửa lại thông tin
+            ViewBag.falid = new SelectList(db.Faculties, "id", "name");
+            return View(model);
         }
 
         // GET: admin/Dentists/Edit/5
@@ -70,33 +172,118 @@ namespace Dentalcare.Areas.admin.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
+
+            // Find the dentist record
             Dentist dentist = db.Dentists.Find(id);
             if (dentist == null)
             {
                 return HttpNotFound();
             }
+
+            // Create a view model that includes the dentist data
+            var viewModel = new CreateDentistViewModel
+            {
+                dentist = dentist,
+                person = db.People.Find(dentist.id),
+                account = db.Accounts.Find(dentist.id) // Adjust as necessary to map account data
+            };
+
+            // Populate the dropdown lists for faculty and person data
             ViewBag.falid = new SelectList(db.Faculties, "id", "name", dentist.falid);
             ViewBag.id = new SelectList(db.People, "id", "meta", dentist.id);
-            return View(dentist);
+
+            return View(viewModel);
         }
+
 
         // POST: admin/Dentists/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "title,hide,meta,order,datebegin,id,falid,descrip")] Dentist dentist)
+        public ActionResult Edit(CreateDentistViewModel model, HttpPostedFileBase avatar)
         {
-            if (ModelState.IsValid)
+            try
             {
-                db.Entry(dentist).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                if (ModelState.IsValid)
+                {
+                    // Cập nhật Dentist
+                    var dentist = db.Dentists.Find(model.dentist.id);
+                    if (dentist == null)
+                    {
+                        return HttpNotFound();
+                    }
+
+                    dentist.title = model.dentist.title;
+                    dentist.hide = model.dentist.hide;
+                    dentist.meta = model.dentist.meta;
+                    dentist.falid = model.dentist.falid;
+                    dentist.descrip = model.dentist.descrip;
+                    dentist.datebegin = DateTime.UtcNow;
+
+                    // Cập nhật Person
+                   var person = db.People.Find(model.dentist.id);
+                    if (person != null)
+                    {
+                        person.name = model.person.name;
+                        person.phoneNumber = model.person.phoneNumber;
+                        person.email = model.person.email;
+                        person.salary = model.person.salary;
+                        person.address = model.person.address;
+                        person.gender = model.person.gender;
+                        person.birthday = model.person.birthday;
+                        person.nation = model.person.nation;
+
+                        System.Diagnostics.Debug.WriteLine(person.birthday);
+                        // Xử lý upload file avatar nếu có
+                        if (avatar != null && avatar.ContentLength > 0)
+                        {
+                            string fileExtension = Path.GetExtension(avatar.FileName);
+                            string folderPath = Server.MapPath($"~/Content/images/{person.id}/");
+
+                            if (!Directory.Exists(folderPath))
+                                Directory.CreateDirectory(folderPath);
+
+                            string avatarPath = $"/Content/images/{person.id}/avatar{fileExtension}";
+                            avatar.SaveAs(Path.Combine(folderPath, $"avatar{fileExtension}"));
+                            person.img = avatarPath; // Cập nhật đường dẫn avatar mới
+                        }
+                    }
+
+                    // Cập nhật Account (nếu có chỉnh sửa)
+                    var account = db.Accounts.Find(model.dentist.id);
+                    if (account != null)
+                    {
+                        account.username = model.account.username;
+                        account.able = model.account.able;
+                    }
+
+                    db.SaveChanges(); // Lưu tất cả thay đổi vào database
+                    return RedirectToAction("Index");
+                }
+                // Nếu ModelState không hợp lệ
+                ViewBag.falid = new SelectList(db.Faculties, "id", "name", model.dentist.falid);
+                return View(model);
             }
-            ViewBag.falid = new SelectList(db.Faculties, "id", "name", dentist.falid);
-            ViewBag.id = new SelectList(db.People, "id", "meta", dentist.id);
-            return View(dentist);
+            catch (DbEntityValidationException e)
+            {
+                foreach (var validationErrors in e.EntityValidationErrors)
+                {
+                    foreach (var validationError in validationErrors.ValidationErrors)
+                    {
+                        System.Diagnostics.Debug.WriteLine($"Property: {validationError.PropertyName}, Error: {validationError.ErrorMessage}");
+                    }
+                }
+
+                // Ghi thêm log nếu cần
+                System.Diagnostics.Debug.WriteLine("Full exception: " + e);
+
+                throw; // Không cần truyền lại 'e' vào throw, chỉ cần throw là đủ
+            }
+
+
         }
+
 
         // GET: admin/Dentists/Delete/5
         public ActionResult Delete(string id)
@@ -119,7 +306,11 @@ namespace Dentalcare.Areas.admin.Controllers
         public ActionResult DeleteConfirmed(string id)
         {
             Dentist dentist = db.Dentists.Find(id);
+            Person person = db.People.Find(id);
+            Account account = db.Accounts.Find(id);
             db.Dentists.Remove(dentist);
+            db.People.Remove(person);
+            db.Accounts.Remove(account);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
