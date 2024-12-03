04b0e08ac38f0c486ab79497bf882d3822a53fec
Tạ Triết <138562042+TaTriet1412@users.noreply.github.com>
2024-12-04 01:59:47 +0700

    add prescription

---
+++ Dentalcare/Areas/admin/Controllers/BillsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Dentalcare.Models;
+
+namespace Dentalcare.Areas.admin.Controllers
+{
+    public class BillsController : Controller
+    {
+        private clinicEntities db = new clinicEntities();
+
+        // GET: admin/Bills
+        public ActionResult Index()
+        {
+            var bills = db.Bills.Include(b => b.Patient);
+            return View(bills.ToList());
+        }
+
+        // GET: admin/Bills/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bill bill = db.Bills.Find(id);
+            if (bill == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bill);
+        }
+
+        // GET: admin/Bills/Create
+        public ActionResult Create()
+        {
+            ViewBag.patid = new SelectList(db.Patients, "id", "meta");
+            return View();
+        }
+
+        // POST: admin/Bills/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,totalPrice,ServicesPrice,PrescriptionPrice,able,hide,meta,order,datebegin,isPayed,patid")] Bill bill)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Bills.Add(bill);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.patid = new SelectList(db.Patients, "id", "meta", bill.patid);
+            return View(bill);
+        }
+
+        // GET: admin/Bills/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bill bill = db.Bills.Find(id);
+            if (bill == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.patid = new SelectList(db.Patients, "id", "meta", bill.patid);
+            return View(bill);
+        }
+
+        // POST: admin/Bills/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,totalPrice,ServicesPrice,PrescriptionPrice,able,hide,meta,order,datebegin,isPayed,patid")] Bill bill)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(bill).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.patid = new SelectList(db.Patients, "id", "meta", bill.patid);
+            return View(bill);
+        }
+
+        // GET: admin/Bills/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bill bill = db.Bills.Find(id);
+            if (bill == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bill);
+        }
+
+        // POST: admin/Bills/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Bill bill = db.Bills.Find(id);
+            db.Bills.Remove(bill);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
